7e654c8c32af8a0203f7f2b77db3a516ba763a7a
heathervv <heathervandervecht@gmail.com>
2018-09-23 15:57:03 -0400

    Fix answer to 1.6

--- Chapter 1/1.1/1.6.scm
+++ Chapter 1/1.1/1.6.scm
@@ -24,4 +24,4 @@
 
 ; What happens when Alyssa attempts to use this to compute square roots? Explain.
 
-; If is a special form - it doesn't use applicative-order to evaluate, it uses normal-order. Because of this, when using `cond` instead of `if` for sqrt-iter will result in a stack overflow. In detail, that's because cond won't evaluate the predicate to it's simplest form (answer) therefore never reaching the true clause before erroring out.
+; If is a special form - it doesn't use applicative-order to evaluate, it uses normal-order. cond uses applicative-order, which means it will evaluate the predicate functions for each clause to determine which consequent expression to evaluate and return. TLDR applicative-order evaluates subexpressions. This means that by using cond instead of if, the procedure will be stuck in a recursive loop and result in a stack overflow. if, using normal-order (aka lazy evaluation) only initiates recursion if the predicate returns false.
